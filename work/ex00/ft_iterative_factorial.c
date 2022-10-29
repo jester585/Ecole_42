536ba55df091d90594f49923d69664c2a18fdd0f
merto <mertbayraktar585@gmail.com>
2022-10-29 13:42:42 +0300

    ses

--- work/ex00/ft_iterative_factorial.c
+++ work/ex00/ft_iterative_factorial.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asdfg585 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 13:38:35 by asdfg585          #+#    #+#             */
+/*   Updated: 2022/10/29 13:39:06 by asdfg585         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int	ft_iterative_factorial(int	nb)
+int	ft_iterative_factorial(int nb)
 {
 	unsigned int	result;
 
 	result = 1;
 	while (nb > 0)
 	{
-		 result *= nb;
+		result *= nb;
 		nb--;
 	}
 	if (nb < 0)
