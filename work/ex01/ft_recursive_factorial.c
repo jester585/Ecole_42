24cc24c55f47b6609c74a7cb38e7dfe5da94ceae
Ömer Mert BAYRAKTAR <42076112+jester585@users.noreply.github.com>
2022-10-29 13:04:09 +0300

    Update ft_recursive_factorial.c

--- work/ex01/ft_recursive_factorial.c
+++ work/ex01/ft_recursive_factorial.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int	ft_recursive_factorial(int	nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb > 0)
+	{
+		result *= nb * ft_recursive_factorial(nb - 1);
+		nb -= 1;
+	}
+	if (nb < 0)
+		return (0);
+	return (result);
+}
