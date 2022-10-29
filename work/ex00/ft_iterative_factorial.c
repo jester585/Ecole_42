0c75da7faa96c3007a2bad6856de57f909ecf46b
Ömer Mert BAYRAKTAR <42076112+jester585@users.noreply.github.com>
2022-10-29 13:03:35 +0300

    Update ft_iterative_factorial.c

--- work/ex00/ft_iterative_factorial.c
+++ work/ex00/ft_iterative_factorial.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int	ft_iterative_factorial(int	nb)
+{
+	unsigned int	result;
+
+	result = 1;
+	while (nb > 0)
+	{
+		 result *= nb;
+		nb--;
+	}
+	if (nb < 0)
+	{
+		return (0);
+	}
+	return (result);
+}
